33463f19b401e8f71a9653d33dd16d3839f17511
liuxuefeng1997 <m15847194612@163.com>
2025-11-12 00:07:08 +0800

    为Mod更新添加全球加速域名支持，默认关闭

--- data/api_setting.py
+++ data/api_setting.py
@@ -7,15 +7,20 @@ class TencentCloud:
         scheme = "https"
 
     class Update:
+        # 本体更新
         update_region = "ap-shanghai"
         update_bukkit = "update-1251534239"
         update_cosDir = "repo_mod_updater"
         update_manifest = "gui_version.json"
         update_channel_list = "gui_channel.json"
-        mod_region = "ap-beijing"
-        mod_bukkit = "repo-1251534239"
 
         self_update_manifest_url = "https://"f"{update_bukkit}.cos.{update_region}.myqcloud.com/{update_cosDir}/{update_manifest}"
         self_update_url = "https://"f"{update_bukkit}.cos.{update_region}.myqcloud.com/{update_cosDir}/"
-        mod_update_url = "https://"f"{mod_bukkit}.cos.{mod_region}.myqcloud.com/"
         self_update_channel_list_url = "https://"f"{update_bukkit}.cos.{update_region}.myqcloud.com/{update_cosDir}/{update_channel_list}"
+
+        # Mod更新
+        mod_region = "ap-beijing"
+        mod_bukkit = "repo-1251534239"
+        enable_global = False  # 是否启用全球加速域名
+        mod_update_url = "https://"f"{mod_bukkit}.cos.{'accelerate' if enable_global else mod_region}.myqcloud.com/"
+
